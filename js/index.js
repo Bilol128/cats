34e7add49e7bec749db06dcb9b5bdcd9eb550195
bilol <bilolzomolov@gmail.com>
2022-06-04 07:33:41 +0500

    567

--- js/index.js
+++ js/index.js
@@ -4,14 +4,18 @@ const input = document.querySelector('.result__input')
 const cats_before = document.querySelector('.before-cats')
 const cats_beforeImage = document.querySelector('.before-cats__image')
 const loader = document.querySelector('.loader')
+const body = document.querySelector('body')
+const nav = document.querySelector('.navbar')
 
 button.addEventListener('click' , function(){
     if(!menu.classList.contains('active')){
         menu.classList.add('active')
         button.classList.add('activeButton')
+        body.classList.add('body-overflow')
     }else{
         menu.classList.remove('active')
         button.classList.remove('activeButton')
+        body.classList.remove('body-overflow')
     }
 })
 
@@ -33,3 +37,11 @@ document.body.onload = function() {
         }
     }, 1000);
 }
+
+window.addEventListener('scroll' , function(){
+    if(scrollY  > 1 ){
+      nav.classList.add('navbar-fixed')
+    }else{
+        nav.classList.remove('navbar-fixed')
+    }
+})
